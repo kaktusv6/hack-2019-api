3853c34ceaf18a9d5695203a9bed2c5a7f844182
Vasiliy <kaktusv6@gmail.com>
2019-06-09 00:12:24 +1000

    Добавил настройки для подключения к БД

---
+++ config/db.php
@@ -0,0 +1,21 @@
+<?php
+
+class Database {
+  static $mysql = null;
+
+  static $host = 'localhost';
+  static $username = 'kaktusv6_hack';
+  static $password = 'Mamont500';
+  static $database = 'kaktusv6_hack';
+
+  static private function initConnection() {
+    if (!self::$mysql) {
+      self::$mysql = new mysqli(self::$host, self::$username, self::$password, self::$database);
+    }
+  }
+
+  static function getMySql() {
+    self::initConnection();
+    return self::$mysql;
+  }
+}
