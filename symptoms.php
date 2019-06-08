20f9b07df2cb51101a52268b0761525bc830d326
Vasiliy <kaktusv6@gmail.com>
2019-06-09 00:12:52 +1000

    Реализовал api для выдачи симптомов

---
+++ symptoms.php
@@ -0,0 +1,17 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+require 'config/debug.php';
+require 'config/db.php';
+
+$mysql = Database::getMySql();
+
+$query = "select distinct name from symptoms";
+$res = $mysql->query($query);
+$arSymptons = [];
+while ($row = mysqli_fetch_assoc($res)) {
+  $arSymptons[] = $row;
+}
+
+echo json_encode($arSymptons);
